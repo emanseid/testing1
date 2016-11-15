06e5d2b8f4b44c4f44180ccc00f6f4f97ade3531
emanseid <emanseid@gmail.com>
2016-11-14 19:43:16 -0500

    initial check

---
+++ Chapter 5/src/edu/gsu/cis/ch5/Question3.java
@@ -0,0 +1,45 @@
+package edu.gsu.cis.ch5;
+import java.util.Scanner;
+
+public class Question3 {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("enter a number");
+
+		int a = input.nextInt();
+
+		// calling method
+		// value of a needs to be returned
+
+		
+
+		boolean result = isPalindrome(a);
+		if(result == true)
+		System.out.println("Palindrome");
+		else
+		System.out.println("Not Palindrome");
+
+	}
+
+	public static boolean isPalindrome(int number){
+		if(number == reverse(number))
+		return true;
+		else
+		return false;
+	}
+
+	public static int reverse(int number) {
+		int temp = 0;
+		int reversedNumber = 0;
+		while (number > 0) {
+			temp = number % 10;
+			number = number / 10;
+			reversedNumber = reversedNumber * 10 + temp;
+		}
+		return reversedNumber;
+
+	}
+
+}
